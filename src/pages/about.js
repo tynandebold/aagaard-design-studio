9959d2603b3bf035639c48252170f5eb6cb9f90b
Tynan DeBold <thdebold@gmail.com>
2019-07-22 11:34:15 +0200

    use contentful to populate info page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,39 +1,49 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+import { INLINES } from "@contentful/rich-text-types"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Nav from "../components/nav"
 
-const AboutPage = () => (
-  <Layout darkTheme={false}>
-    <SEO title="about" />
-    <section className="left">
-      <Nav page="about" />
-    </section>
-    <section className="right">
-      <p>
-        A Copenhagen–based, one-man army design studio. I design, advise and
-        carry out purposeful solutions — as a brand strategist, futurist and
-        self–taught designer. Currently working as a Senior Designer at Charlie
-        Tango. Educationally I hold a Bachelors degree in Social Science and a
-        Masters in E-business. I have comprehensive experience in working with
-        the digitalisaion of brands — from brand values and visual identity to
-        digital product design and design systems. My philosophy is to think
-        brand when doing digital and to think digital when doing brand. I don’t
-        believe in any certain process or methodology, nor do I religiously
-        worship the latest set of buzzwords. I believe in simplicity, expertise,
-        intuition, effort, logic, collaboration and trial and error — and above
-        all I believe in whatever it takes.
-      </p>
-      <p>
-        Client list: Danske Bank, Lauritz.com, SAS, Libratone, Coloplast,
-        Wilhelm Lauritz Arkitekter, Dinesen, YouSee, Virk, Borger, Storebælt AS,
-        3 Days of Design, Velux, Gether Conemporary, ABN Amro, Anker&co,
-        Scalepoint + more.
-      </p>
-      <p className="date">26.06––2019</p>
-    </section>
-  </Layout>
-)
+const AboutPage = () => {
+  const { contentfulInfo: data } = useStaticQuery(graphql`
+    {
+      contentfulInfo {
+        infoText {
+          json
+        }
+      }
+    }
+  `)
+
+  return (
+    <Layout darkTheme={false}>
+      <SEO title="about" />
+      <section className="left">
+        <Nav page="about" />
+        {console.log(data)}
+      </section>
+      <section className="right">
+        {documentToReactComponents(data.infoText.json, {
+          renderNode: {
+            [INLINES.HYPERLINK]: (node, children) => (
+              <a
+                className="info-link"
+                href={node.data.uri}
+                rel="noopener"
+                target="_blank"
+              >
+                {children}
+              </a>
+            ),
+          },
+        })}
+        <p className="date">26.06––2019</p>
+      </section>
+    </Layout>
+  )
+}
 
 export default AboutPage
